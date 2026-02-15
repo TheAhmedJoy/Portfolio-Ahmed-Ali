9566c6ab4248ea22fffb6be219f95a664c1e30ee
Ahmed Ali <theahmedjoyali@gmail.com>
2026-02-14 17:35:00 -0800

    Added a background hue to the Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,11 +5,14 @@ import { assets } from "../../assets//assets"
 export default function Navbar() {
     return (
         <>
+            <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+                <Image src={assets.header_bg_color} className="w-full" alt="Navbar background" />
+            </div>
             <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-around z-50">
                 <a href="#top" >
                     <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="Navbar Logo" />
                 </a>
-                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3">
+                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm backdrop-opacity-50">
                     <li>
                         <a className="font-Ovo" href="#top">Home</a>
                     </li>
