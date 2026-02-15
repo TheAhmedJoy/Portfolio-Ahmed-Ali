09330aff4950dc4d7078aea294899fd3a15624ee
Ahmed Ali <theahmedjoyali@gmail.com>
2026-02-14 18:03:28 -0800

    Added opening and close side nav bar functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Image from 'next/image'
 import { assets } from "../../assets//assets"
 
 export default function Navbar() {
+
+    const sideNavRef: any = useRef<HTMLUListElement | null>(null)
+
+    const openSideNav = () => {
+        sideNavRef.current.style.transform = "translateX(-16rem)"
+    }
+
+    const closeSideNav = () => {
+        sideNavRef.current.style.transform = "translateX(16rem)"
+    }
+
     return (
         <>
             <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -28,33 +39,34 @@ export default function Navbar() {
                 </ul>
                 <div className="flex items-center gap-4">
                     <button>
-                        <Image src={assets.moon_icon} className="w-6" alt="Dark Mode Icon" />
+                        <Image src={assets.moon_icon} className="w-6 cursor-pointer" alt="Dark Mode Icon" />
                     </button>
                     <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-14 font-Ovo">
                         Contact
                         <Image src={assets.arrow_icon} className="w-3" alt="Contact Me arrow" />
                     </a>
                     <button className="block md:hidden ml-3">
-                        <Image src={assets.menu_black} className="w-6" alt="Burger Menu Icon" />
+                        <Image src={assets.menu_black} className="w-6 cursor-pointer" alt="Burger Menu Icon" onClick={openSideNav}/>
                     </button>
                 </div>
 
                 {/*Small Screen Navrbar*/}
-                <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500">
-                    <div className="absolute right-6 top-6">
-                        <Image src={assets.close_black} className="w-5 cursor-pointer" alt="Close Small Screen Navbar"/>
+                <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+                    ref={sideNavRef}>
+                    <div className="absolute right-6 top-6" onClick={closeSideNav}>
+                        <Image src={assets.close_black} className="w-5 cursor-pointer" alt="Close Small Screen Navbar" />
                     </div>
                     <li>
-                        <a className="font-Ovo" href="#top">Home</a>
+                        <a className="font-Ovo" href="#top" onClick={closeSideNav}>Home</a>
                     </li>
                     <li>
-                        <a className="font-Ovo" href="#about">About Me</a>
+                        <a className="font-Ovo" href="#about" onClick={closeSideNav}>About Me</a>
                     </li>
                     <li>
-                        <a className="font-Ovo" href="#word">My Work</a>
+                        <a className="font-Ovo" href="#word" onClick={closeSideNav}>My Work</a>
                     </li>
                     <li>
-                        <a className="font-Ovo" href="#contact">Contact Me</a>
+                        <a className="font-Ovo" href="#contact" onClick={closeSideNav}>Contact Me</a>
                     </li>
                 </ul>
             </nav>
