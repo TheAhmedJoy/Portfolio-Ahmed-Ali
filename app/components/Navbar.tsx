6073bcc43fe8599d769384c3214e4287a4f8bab4
Ahmed Ali <theahmedjoyali@gmail.com>
2026-02-14 19:41:45 -0800

    Updated Navbar centering

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -19,11 +19,11 @@ export default function Navbar() {
             <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
                 <Image src={assets.header_bg_color} className="w-full" alt="Navbar background" />
             </div>
-            <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-around z-50">
+            <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] max-lg:py-8 py-4 flex items-center justify-between z-50">
                 <a href="#top" >
-                    <Image src={assets.logo} className='w-28 cursor-pointer mr-14' alt="Navbar Logo" />
+                    <Image src={assets.logo} className='w-28 cursor-pointer' alt="Navbar Logo" />
                 </a>
-                <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm backdrop-opacity-50">
+                <ul className="hidden md:flex absolute left-1/2 -translate-x-1/2 items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-md backdrop-opacity-50">
                     <li>
                         <a className="font-Ovo" href="#top">Home</a>
                     </li>
@@ -41,7 +41,7 @@ export default function Navbar() {
                     <button>
                         <Image src={assets.moon_icon} className="w-6 cursor-pointer" alt="Dark Mode Icon" />
                     </button>
-                    <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-14 font-Ovo">
+                    <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full font-Ovo">
                         Contact
                         <Image src={assets.arrow_icon} className="w-3" alt="Contact Me arrow" />
                     </a>
