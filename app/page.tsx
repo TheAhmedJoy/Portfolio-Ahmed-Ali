5cffbc2a7a4a3b29b9c51bcb20e8dcd497b8f929
Ahmed Ali <theahmedjoyali@gmail.com>
2026-04-12 14:44:13 -0700

    Added darkmode logic for system preferences and manually toggling darkmode on/off via user input

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,35 @@ import AboutMe from "./components/AboutMe";
 import MyWork from "./components/MyWork";
 import ContactMe from "./components/ContactMe";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      setIsDarkMode(true)
+    }
+    else {
+      setIsDarkMode(false)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark")
+      localStorage.theme = "dark"
+    }
+    else {
+      document.documentElement.classList.remove("dark")
+      localStorage.theme = ""
+    }
+  }, [isDarkMode])
+
   return (
     <>
-      <Navbar />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
       <Header />
       <AboutMe />
       <MyWork />
