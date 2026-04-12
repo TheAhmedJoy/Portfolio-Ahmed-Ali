e20048095823b23de3865de00bad43bb99429ea6
Ahmed Ali <theahmedjoyali@gmail.com>
2026-04-12 14:44:30 -0700

    Added darkmode features to the Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,12 @@ import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { assets } from "../../assets//assets"
 
-export default function Navbar() {
+interface NavbarProps {
+    isDarkMode: boolean;
+    setIsDarkMode: (value: boolean | ((prev: boolean) => boolean)) => void;
+}
+
+export default function Navbar( {isDarkMode, setIsDarkMode}: NavbarProps ) {
 
     const [isScrollStatus, setIsScrollStatus] = useState(false)
 
@@ -29,16 +34,16 @@ export default function Navbar() {
 
     return (
         <>
-            <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+            <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
                 <Image src={assets.header_bg_color} className="w-full" alt="Navbar background" />
             </div>
             <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] max-lg:py-8 py-4 flex items-center justify-between z-50 
-                            ${isScrollStatus ? "bg-white/50 backdrop-blur-lg shadow-sm" : ""}`}>
+                            ${isScrollStatus ? "bg-white/50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
                 <a href="#top" >
-                    <Image src={assets.logo} className='w-28 cursor-pointer' alt="Navbar Logo" />
+                    <Image src={isDarkMode ? assets.logo_dark : assets.logo} className='w-28 cursor-pointer' alt="Navbar Logo" />
                 </a>
                 <ul className={`hidden md:flex absolute left-1/2 -translate-x-1/2 items-center gap-6 lg:gap-8 rounded-full px-12 py-3 
-                                ${isScrollStatus ? "" : "bg-white/50 shadow-md"}`}>
+                                ${isScrollStatus ? "" : "bg-white/50 shadow-md dark:border dark:border-white/50 dark:bg-transparent"}`}>
                     <li>
                         <a className="font-Ovo" href="#top">Home</a>
                     </li>
@@ -53,23 +58,24 @@ export default function Navbar() {
                     </li>
                 </ul>
                 <div className="flex items-center gap-4">
-                    <button>
-                        <Image src={assets.moon_icon} className="w-6 cursor-pointer" alt="Dark Mode Icon" />
+                    <button onClick={() => setIsDarkMode((prev) => !prev)}>
+                        <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} className="w-6 cursor-pointer" alt="Dark Mode Icon" />
                     </button>
-                    <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full font-Ovo">
+                    <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full font-Ovo dark:border-white/50">
                         Contact
-                        <Image src={assets.arrow_icon} className="w-3" alt="Contact Me arrow" />
+                        <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} className="w-3" alt="Contact Me arrow" />
                     </a>
                     <button className="block md:hidden ml-3">
-                        <Image src={assets.menu_black} className="w-6 cursor-pointer" alt="Burger Menu Icon" onClick={openSideNav}/>
+                        <Image src={isDarkMode ? assets.menu_white : assets.menu_black} className="w-6 cursor-pointer" alt="Burger Menu Icon" onClick={openSideNav}/>
                     </button>
                 </div>
 
-                {/*Small Screen Navrbar*/}
-                <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+                {/*Small Screen Navbar*/}
+                <ul className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500
+                            dark:bg-darkHover dark:text-white"
                     ref={sideNavRef}>
                     <div className="absolute right-6 top-6" onClick={closeSideNav}>
-                        <Image src={assets.close_black} className="w-5 cursor-pointer" alt="Close Small Screen Navbar" />
+                        <Image src={isDarkMode ? assets.close_white : assets.close_black} className="w-5 cursor-pointer" alt="Close Small Screen Navbar" />
                     </div>
                     <li>
                         <a className="font-Ovo" href="#top" onClick={closeSideNav}>Home</a>
