3d0770fb79079cfe3905a0b175f5c7cdb4ce80d7
Ahmed Ali <theahmedjoyali@gmail.com>
2026-04-12 14:43:34 -0700

    Added a darkmode background for all components that are part of the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}>
+      <body className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}>
         {children}
       </body>
     </html>
