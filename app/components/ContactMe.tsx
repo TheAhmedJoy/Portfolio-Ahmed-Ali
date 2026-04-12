26a1ac36e039279c06aaf67eb06fae93b2e743e8
Ahmed Ali <theahmedjoyali@gmail.com>
2026-04-12 16:52:19 -0700

    Added Framer Animations to the ContactMe component

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -1,6 +1,7 @@
-import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import { motion } from "motion/react"
+import { assets } from '@/assets/assets'
 
 export default function ContactMe() {
 
@@ -30,34 +31,43 @@ export default function ContactMe() {
     };
 
     return (
-        <div id="contact" className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%-auto] dark:bg-none">
-            <h4 className="text-center mb-2 text-lg font-Ovo">
+        <motion.div initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1}} viewport={{once: true}}
+        id="contact" className="w-full px-[12%] py-10 scroll-mt-20 bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-size-[90%-auto] dark:bg-none">
+            <motion.h4 initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 0.3}} viewport={{once: true}}
+            className="text-center mb-2 text-lg font-Ovo">
                 Connect with me
-            </h4>
-            <h2 className="text-center text-5xl font-Ovo">
+            </motion.h4>
+            <motion.h2 initial={{y: -20, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.5, delay: 0.5}} viewport={{once: true}}
+            className="text-center text-5xl font-Ovo">
                 Get in touch
-            </h2>
-            <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovoa">
+            </motion.h2>
+            <motion.p initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5, delay: 0.7}} viewport={{once: true}}
+            className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovoa">
                 I'm currently seeking new opportunities as a Full Stack Developer! Please contact me using the form below if you like what you see!
-            </p>
-            <form className="max-w-2xl mx-auto" onSubmit={submitContactForm}>
+            </motion.p>
+            <motion.form initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 0.5, delay: 0.9}} viewport={{once: true}}
+            className="max-w-2xl mx-auto" onSubmit={submitContactForm}>
                 <div className="grid grid-cols-2 gap-6 mt-10 mb-8">
-                    <input type="text" placeholder="Enter your name" required
-                        className="p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" name="name"/>
-                    <input type="email" placeholder="Enter your email" required
-                        className="p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" name="email"/>
+                    <motion.input initial={{x: -50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.1}} viewport={{once: true}}
+                    type="text" placeholder="Enter your name" required
+                        className="p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" name="name" />
+                    <motion.input initial={{x: 50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.1}} viewport={{once: true}} 
+                    type="email" placeholder="Enter your email" required
+                        className="p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90" name="email" />
                 </div>
-                <textarea rows={6} placeholder='Enter your message' required
-                    className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90" name="message"></textarea>
-                <button type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 
+                <motion.textarea initial={{y: 100, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.1}} viewport={{once: true}}
+                rows={6} placeholder='Enter your message' required
+                    className="w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90" name="message"></motion.textarea>
+                <motion.button whileHover={{scale: 1.15}} transition={{duration: 0.1}}
+                type="submit" className="py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 
                                     dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover cursor-pointer">
                     Send now
                     <Image src={assets.right_arrow_white} className="w-4" alt="Send arrow icon" />
-                </button>
+                </motion.button>
                 <p className="mt-4">
                     {formResult}
                 </p>
-            </form>
-        </div>
+            </motion.form>
+        </motion.div>
     )
 }
